1b4f5200bf2e766621ddc0eb93b2777871671b80
Vshiwam <officialshiwam28@gmail.com>
2021-09-21 18:28:05 +0530

    Birthday Card

---
+++ app/src/main/java/com/example/birthdaycard/BirthdayGreetingActivity.kt
@@ -0,0 +1,22 @@
+package com.example.birthdaycard
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_birthday_greeting.*
+
+class BirthdayGreetingActivity : AppCompatActivity() {
+
+    companion object{
+        const val NAME_EXTRA = "name_extra"
+    }
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting)
+
+        val name = intent.getStringExtra(NAME_EXTRA)
+        birthdayGreeting.text = "Happy Birthday $name"
+
+    }
+}
---
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.birthdaycard
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {
+
+        val name = nameInput.editableText.toString()
+
+        val intent = Intent(this,BirthdayGreetingActivity::class.java)
+        intent.putExtra(BirthdayGreetingActivity.NAME_EXTRA,name)
+        startActivity(intent)
+    }
+
+}
